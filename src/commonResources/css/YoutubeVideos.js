910a773080814356f55dd26516359a1fca5e4a66
tekea <tekea_angesom@yahoo.com>
2022-08-10 12:06:04 -0500

    first commit

---
+++ src/commonResources/css/YoutubeVideos.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function YoutubeVideos() {
+  return (
+    <div>YoutubeVideos</div>
+  )
+}
+
+export default YoutubeVideos
---
+++ src/components/jq.js
@@ -0,0 +1,12 @@
+
+// import React from 'react';
+import $ from 'jquery';
+$(document).ready(function () {
+    $('h3').click(function () {
+      $(this).nextUntil('h3').toggle();
+      $(this).toggleClass ('active');
+    });
+  });
+  $(window).resize(function () {
+    window.location.reload();
+  });
